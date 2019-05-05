74db2d6df656c6e09255ee77cbefa17f2f457aea
Franchely Portorreal <franchely811@gmail.com>
2019-05-05 00:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var myArray = []
+  for (let i = 0; i < musicians.length; i++) {
+    myArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  } return myArray
+  
+}
+
+function johnLennonFacts(array) {
+ var theArray = array.length 
+  
+}
